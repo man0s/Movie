36ed0bb29e4b9660aef64862a2f3594aab9e480f
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2017-12-12 04:38:06 +0200

    App upload!

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); //serve(expr) the content of the public dir
+
+app.get("/", function(req, res){
+    var data = [];
+    res.render("home", {data: data});
+});
+
+app.post("/search", function(req, res){
+    request('https://www.omdbapi.com/?s=' + req.body.searchedQuery + '&type=movie&apikey=thewdb', function (error, response, body) {
+      if(error) console.log('StatusCode:', response && response.statusCode,'\nError:', error); // Print the error and the response status code if a response was received
+      else { 
+        var data = JSON.parse(body).Search;
+        if(JSON.parse(body).totalResults) {
+            res.render("home", {data: data});
+        }
+        else {
+            res.redirect("/");
+        }
+      }
+    });
+});
+app.get("*", function(req, res){
+    res.render("404");
+});
+
+
+app.listen(3000, '127.0.0.1', function(){
+    console.log('Server started at', process.env.PORT);
+});
